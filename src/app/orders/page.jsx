903b1a84f1161016bd35b7b2ff843df66f23a2cb
Sumitpanchal <sumitpanchal08@gmail.com>
2025-08-28 13:29:24 +0530

    Order Page Corrected

--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -1,19 +1,18 @@
 'use client';
 import React from "react";
 import "./Orders.css";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
+import { useGlobalStore } from '@/Store/GlobalStore';
 
 function Orders() {
-  const redux_cart = useSelector((state) => {
-    return state.redux_cart;
-  });
-
-  console.log(redux_cart);
+  // const redux_cart = useSelector((state) => {return state.redux_cart;});
+const {cartItems} = useGlobalStore();
+  // console.log(redux_cart);
   return (
     <>
       <div className="orderBase">
         <div>
-          {redux_cart.map((item, id) => {
+          {cartItems.map((item, id) => {
             return <h1 key={id}>Added Orders</h1>;
           })}
         </div>
