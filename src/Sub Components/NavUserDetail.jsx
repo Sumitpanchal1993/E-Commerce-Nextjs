2ab8686f151cbe7c7eff05cc69ecafdcd69c8254
Sumitpanchal <sumitpanchal08@gmail.com>
2025-09-06 06:12:10 +0530

    Dropdown Corrected

--- src/Sub Components/NavUserDetail.jsx
+++ src/Sub Components/NavUserDetail.jsx
@@ -8,21 +8,25 @@ function NavUserDetail({ logout }) {
   const { cartItems } = useGlobalStore();
   const [userName, setUserName] = useState("User");
   const [userID, setUserID] = useState("Username_here");
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
 
   // const cartItem = useSelector((state)=> state.redux_cart);
 
+  // const dropdown = () => {
+  //   let status = document.querySelector(".dropdown");
+  //   if (status.style.display == "none") {
+  //     status.style.display = "block";
+  //   } else {
+  //     status.style.display = "none";
+  //   }
+  // };
   const dropdown = () => {
-    let status = document.querySelector(".dropdown");
-    if (status.style.display == "none") {
-      status.style.display = "block";
-    } else {
-      status.style.display = "none";
-    }
-  };
+  setDropdownVisible(!isDropdownVisible);
+};
 
   return (
     <>
-       <button className="dropdown_btn badge">
+       <button className="cart_btn badge">
         <Link href="/my_cart">
           <img src="/Media/Cart.png" alt="" />
         </Link>
@@ -38,25 +42,15 @@ function NavUserDetail({ logout }) {
         <span className="material-symbols-outlined">arrow_drop_down</span>
       </button>
 
-      <div className="dropdown">
-        <ul>
-          <li>
-            <Link href="/myprofile">My Profile</Link>
-          </li>
-          <li>
-            <Link href="/myorder">Orders</Link>
-          </li>
-          <li>
-            <Link href="/my_cart">Cart</Link>
-          </li>
-          <li>
-            <Link href="#">Wishlist</Link>
-          </li>
-          <li onClick={logout}>
-            <Link href="/">Logout</Link>
-          </li>
-        </ul>
-      </div>
+      <div className={`dropdown ${isDropdownVisible ? "show" : ""}`}>
+  <ul>
+    <li><Link href="/myprofile">My Profile</Link></li>
+    <li><Link href="/orders">Orders</Link></li>
+    <li><Link href="/my_cart">Cart</Link></li>
+    <li><Link href="#">Wishlist</Link></li>
+    <li onClick={logout}><Link href="/">Logout</Link></li>
+  </ul>
+</div>
     </>
   );
 }
